eff91c46d03d3d06b2360d166a6bbf359b40288a
Szabo Ștefan Raul <stefiszabo04@gmail.com>
2024-09-07 11:27:31 +0000

    Uninstalled @emotion/react, @emotion/styled, @fontsource/roboto, @mui/icons-material, @mui/material

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
+import '@material/web/all.js';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <md-elevated-button>Material 3 Button</md-elevated-button>
       </header>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -3,15 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import '@fontsource/roboto/300.css';
-import '@fontsource/roboto/400.css';
-import '@fontsource/roboto/500.css';
-import '@fontsource/roboto/700.css';
-import Button from '@mui/material/Button';
 
-export default function ButtonUsage() {
-  return <Button variant="contained">Hello World</Button>;
-}
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
